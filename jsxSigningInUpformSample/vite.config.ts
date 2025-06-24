d01e69478b4e5b259df2cbb95f318e7fcc5fcb44
Hagop Almadjian <hagop1isaboss@hotmail.com>
2025-06-24 00:37:31 -0700

    done

--- jsxSigningInUpformSample/vite.config.ts
+++ jsxSigningInUpformSample/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jsx_program_chris-s-_practice_one',
   plugins: [tailwindcss(),
     react()
   ],
